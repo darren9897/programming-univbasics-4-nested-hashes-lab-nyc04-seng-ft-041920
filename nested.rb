164793869cf6accb9b1f7cf35ab133f8d85ade94
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-25 20:16:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
       }
     }
 
+return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +37,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+    return programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +58,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    hi = programmer_hash[:dennis_ritchie][:languages]
+    return hi
 
 end
 
@@ -81,6 +87,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash = {:yukihiro_matsumoto => {
+      :known_for => "Ruby", 
+      :languages => ["LISP", "C"]}}
+    
+    return programmer_hash
 
 end
 
@@ -103,6 +115,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    
+    return programmer_hash
 
 end
 
@@ -125,5 +141,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    
+    programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+    return programmer_hash
 
 end
